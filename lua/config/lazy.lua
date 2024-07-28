7abb403051248939766101e8af596221a86efee7
Jeison Redondo <jeida16@yahoo.es>
2024-07-28 14:57:31 -0500

    [ADD](28/7/24)Agregando cambios a la configuracion de nvim, incluyendo pkugin para consultar rest apis

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,7 +11,7 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim",       import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { "EdenEast/nightfox.nvim" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
@@ -29,7 +29,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "nightfox" } },
+  install = { colorscheme = { "tokyonight", "habamax" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
