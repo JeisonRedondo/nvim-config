f798dcc29ee1e4b7cddf11e5fc707d778f2a210a
Jeison Redondo <jeida16@yahoo.es>
2024-09-08 19:03:07 -0500

    [CHANGE]Configurando el colorscheme como fluorecent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -13,32 +13,28 @@ return {
     "maxmx03/fluoromachine.nvim",
     config = function()
       local fm = require("fluoromachine")
-      fm.setup {
-  glow = true,
-  theme = 'retrowave',
-  colors = function(_, color)
-    local darken = color.darken
-    local lighten = color.lighten
-    local blend = color.blend
-    local shade = color.shade
-    local tint = color.tint
-    return {
-      bg = '#190920',
-      bgdark = darken('#190920', 20),
-      cyan = '#49eaff',
-      red = '#ff1e34',
-      yellow = '#ffe756',
-      orange = '#f38e21',
-      pink = '#ffadff',
-      purple = '#9544f7',
-    }
-  end,
-}
-
-      --vim.cmd.colorscheme("fluoromachine")
+      fm.setup({
+        glow = true,
+        theme = "retrowave",
+        colors = function(_, color)
+          local darken = color.darken
+          local lighten = color.lighten
+          local blend = color.blend
+          local shade = color.shade
+          local tint = color.tint
+          return {
+            bg = "#190920",
+            bgdark = darken("#190920", 20),
+            cyan = "#49eaff",
+            red = "#ff1e34",
+            yellow = "#ffe756",
+            orange = "#f38e21",
+            pink = "#ffadff",
+            purple = "#9544f7",
+          }
+        end,
+      })
+      vim.cmd.colorscheme("fluoromachine")
     end,
   },
-  {
-    "LazyVim/LazyVim",
-    opts = {
 }
