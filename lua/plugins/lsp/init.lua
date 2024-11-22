e0965f12f64ea4a117d0d517381c02bc648c996c
Jeison Redondo <jeida16@yahoo.es>
2024-11-22 17:05:05 -0500

    [ADD]Agregando ultimos cambios en la rama main

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -3,10 +3,11 @@ return {
   opts = function()
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
     -- change a keymap
-    keys[#keys + 1] = { "K", "<cmd>echo 'hello'<cr>" }
+    --keys[#keys + 1] = { "K", "<cmd>echo 'hello'<cr>" }
     -- disable a keymap
-    keys[#keys + 1] = { "K", false }
+    --keys[#keys + 1] = { "K", false }
     -- add a keymap
-    keys[#keys + 1] = { "H", "<cmd>echo 'hello'<cr>" }
+    --keys[#keys + 1] = { "H", "<cmd>echo 'hello'<cr>" }
+    -- configurar servidor html
   end,
 }
